e378c0e50111506c2429d09e46354d80ae7ce244
Jinyoung Kim <jinykim@c3r2s7.42seoul.kr>
2020-05-27 18:58:34 +0900

    append run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,10 @@
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+rm -rf ~/.vim
+mkdir ~/.vim
 mv .promptline ~/.promptline
 mv .zshrc ~/.zshrc
-mv .vim ~/.vim
+mv .vim/* ~/.vim/
 mv .vimrc ~/.vimrc
+
