d77733bf7eccf6727c0b95e7e5d34acd75c5e698
Jinyoung Kim <jinykim@c3r2s7.42seoul.kr>
2020-05-27 18:16:35 +0900

    add files

---
+++ run.sh
@@ -0,0 +1,6 @@
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+mv .promptline ~/.promptline
+mv .zshrc ~/.zshrc
+mv .vim ~/.vim
